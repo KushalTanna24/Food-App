0b345ddef05cc02d702aade00b860645a0e90d6c
Kushal Tanna <202012063@daiict.ac.in>
2022-05-27 17:48:22 +0530

    Cart modal-backdrop working with states defined

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,38 @@
+import classes from "./Modal.module.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { Fragment } from "react/cjs/react.production.min";
+import { createPortal } from "react-dom/cjs/react-dom.production.min";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlay");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+      <ModalOverlay />
+    </Fragment>
+  );
+};
+
+export default Modal;
